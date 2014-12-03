8b6f91962e4ca7f3cf6918e1a8493eb1d7983dd7
Marcin Kulesza <Marcin Kulesza@pc2>
2014-12-03 23:45:31 +0100

    clicker

--- TestyMadaFakaFo/src/pakiet/Clicker.java
+++ TestyMadaFakaFo/src/pakiet/Clicker.java
@@ -1,5 +1,24 @@
 package pakiet;
 
-public class Clicker {
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+
+
+public class Clicker extends Robot {
+
+	//ArrayList<int[][]> list = new ArrayList<int[][]>();
+	
+	public Clicker() throws AWTException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void klick(int x, int y)
+	{	
+		mouseMove(x, y);
+		mousePress(InputEvent.BUTTON1_MASK);
+	}
+	
 
 }
